93b8dd508815b5302508640c3ac0af8fe182fcfb
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-07 11:02:06 +0530

    printing patterns today

---
+++ square.py
@@ -0,0 +1,6 @@
+num : int = int (input ('Enter the number of rows and columns in your desired star: '))
+for i in range (1, num +1):
+    for j in range (1, num +1):
+       print ('*' , end = ' ')
+    print ()
+
